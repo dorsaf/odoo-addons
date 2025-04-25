48d65a134a18fe8a5990e35d5a1bf8b0499369dc
dorsaf <dhouibi.dorsaf@gmail.com>
2025-04-25 12:21:52 +0200

    create new method

---
+++ module_4/test3.py
@@ -0,0 +1,9 @@
+def greet(name):
+    """
+    A simple method to greet a user by their name.
+    """
+    return f"Hello, {name}!"
+
+# Example usage
+if __name__ == "__main__":
+    print(greet("Dorsaf"))
